8d779e0ba19400c006fb1c52a7c2cae63c248799
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-05-15 23:34:49 +0300

    Updated 15.05.2021 - 23:34 Ders:44

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,60 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez");
+            RuleFor(x => x.WriterSurName).NotEmpty().WithMessage("Yazar soyadı boş geçilemez");
+            RuleFor(x => x.WriterSurName).MinimumLength(2).WithMessage("Yazar soyadı 2 karakterden küçük olamnaz");
+            RuleFor(x => x.WriterSurName).MaximumLength(50).WithMessage("Yazar soyadı 2 karakterden küçük olamnaz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkında kısmı boş geçilemez");
+            RuleFor(x => x.WriterAbout).Must(IsAboutValid).WithMessage("Hakkında kısmında en az bir defa a harfi kullanılmalıdır");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail kısmı boş geçilemez");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez");
+            RuleFor(x => x.WriterPassword).Must(IsPasswordValid).WithMessage("Parola en az 6 karakter olmalıdır.En az bir harf ve bir sayı içermelidir");
+        }
+
+        private bool IsAboutValid(string arg)
+        {
+            try
+            {
+                Regex regex = new Regex(@"^(?=.*[a,A])");
+                return regex.IsMatch(arg);
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        private bool IsPasswordValid(string arg)
+        {
+            try
+            {
+                Regex regex = new Regex(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$");
+                return regex.IsMatch(arg);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool IsContainsValid(string name)
+        {
+            bool result = name.Contains("a");
+            return result;
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -0,0 +1,71 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterController : Controller
+    {
+        // GET: Writer
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var writervalue = wm.GetList();
+            return View(writervalue);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+       
+        public ActionResult DeleteWriter(int id)
+        {
+            var writervalue = wm.GetById(id);
+            wm.WriterDelete(writervalue);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writervalue = wm.GetById(id);
+            return View(writervalue);
+        }
+
+        [HttpPost]
+        public ActionResult EditWriter(Writer p)
+        {
+            wm.WriterUpdate(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
