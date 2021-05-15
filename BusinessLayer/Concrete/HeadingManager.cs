13bbc51dbfbb2314a9c0efd2c92a574c2a2523f1
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-05-15 19:26:49 +0300

    Add files via upload

---
+++ BusinessLayer/Concrete/HeadingManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class HeadingManager: IHeadingService
+    {
+        IHeadingDal _headingDal;
+
+        public HeadingManager(IHeadingDal headingDal)
+        {
+            _headingDal = headingDal;
+        }
+
+        public List<Heading> GetList()
+        {
+            return _headingDal.List();
+        }
+    }
+}
