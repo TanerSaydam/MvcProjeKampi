f91f610c38d022e3f2247a5f9db17534bec31da3
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-06-17 01:16:18 +0300

    update 17.06.2021 - 01:16 ders:76

---
+++ MvcProjeKampi/Controllers/ContactController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    [Authorize]
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactDal());
+        MessageManager mm = new MessageManager(new EfMessageDal());
+        ContactValidator cv = new ContactValidator();
+        public ActionResult Index()
+        {
+            var contactvalues = cm.GetList();
+            return View(contactvalues);
+        }
+
+        public PartialViewResult GelenKutuSolMenu()
+        {
+            var contactvalues = cm.GetList();
+            ViewBag.sayi = contactvalues.Count();
+
+            var deger1 = mm.GetListInbox();
+            ViewBag.sayi1 = deger1.Count();
+
+            var deger2 = mm.GetListSendbox();
+            ViewBag.sayi2 = deger2.Count();
+
+            var deger3 = mm.GetListDraft();
+            ViewBag.sayi3 = deger3.Count();
+
+            var deger4 = mm.GetListTrash();
+            ViewBag.sayi4 = deger4.Count();
+
+            var deger5 = mm.GetReadList();
+            ViewBag.sayi5 = deger5.Count();
+
+            var deger6 = mm.GetUnReadList();
+            ViewBag.sayi6 = deger6.Count();
+            return PartialView();
+            
+        }
+
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactvalues = cm.GetByID(id);
+            return View(contactvalues);
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,127 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    [Authorize]
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        HeadingValidator HeadingValidator = new HeadingValidator();
+        
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.WriterName + " " + x.WriterSurName,
+                                                      Value = x.WriterID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
+            ViewBag.wrt = valuewriter;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            ValidationResult results = HeadingValidator.Validate(p);
+            if (results.IsValid)
+            {
+                p.HeadingStatus = true;
+                p.HeadingDate = DateTime.Now;
+                hm.HeadingAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult DeleteHeading(int id)
+        {
+            var headingvalues = hm.GetByID(id);
+            if (headingvalues.HeadingStatus == true)
+            {
+                headingvalues.HeadingStatus = false;
+                hm.HeadingDelete(headingvalues);
+            }
+            else
+            {
+                headingvalues.HeadingStatus = true;
+                hm.HeadingDelete(headingvalues);
+            }            
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditHeading(int id)
+        {
+            var headingvalues = hm.GetByID(id);
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            List<SelectListItem> valuewriter = (from x in wm.GetList()
+                                                select new SelectListItem
+                                                {
+                                                    Text = x.WriterName + " " + x.WriterSurName,
+                                                    Value = x.WriterID.ToString()
+                                                }).ToList();
+            ViewBag.vlc = valuecategory;
+            ViewBag.wrt = valuewriter;
+            return View(headingvalues);
+        }
+
+        [HttpPost]
+        public ActionResult EditHeading(Heading p)
+        {
+            ValidationResult results = HeadingValidator.Validate(p);
+            if (results.IsValid)
+            {
+                hm.HeadingUpdate(p);                
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        
+    }
+}
